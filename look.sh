94c4d72f3667799eb67e80757863ee96fc7f4d22
Tim Field <tim@mohiohio.com>
2022-12-01 12:55:45 +1300

    resolution issue

--- look.sh
+++ look.sh
@@ -13,7 +13,7 @@ duration=${1:-1200}
 loop=$((duration < 1200 ? duration : 1200))
 
 while [ $duration -gt 0 ]; do
-	fswebcam -S 20 -p YUYV -r 2592x1944 --save ./working/image-current.jpg
+	fswebcam -S 20 -p YUYV -r 2048x1536 --save ./working/image-current.jpg
 	file="pic-$(date +%Y-%m-%dT%H-%M-%S).jpg"
 	mv ./working/image-current.jpg ./samples/images/$file
 	rsync --remove-source-files -vP ./samples/images/$file tim@mohiohio.com:buzzy/samples/images/$file &
