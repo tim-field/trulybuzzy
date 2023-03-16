a595a1b61c86c11ddd65586adb39bc741d9330a5
Tim Field <field.tim@gmail.com>
2023-03-16 15:28:12 +1300

    lint

--- upload.sh
+++ upload.sh
@@ -1,61 +1,39 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-#echo "startup usb"
-#/usr/sbin/uhubctl -l 1-1 -p 2 -a on
-#sleep 90
+set -e
 
 # also cleared in cron on reboot
 PIDFILE=./upload.pid
 
 trap "rm -f $PIDFILE; echo Uploader Exited!; exit;" SIGINT SIGTERM
 
-# ensure single instance 
+# ensure single instance
 # https://gist.github.com/darth-veitcher/f47eb0a52ae42a1c5e9a65adca460723
-if [ -f $PIDFILE ]
-then
+if [ -f $PIDFILE ]; then
   PID=$(cat $PIDFILE)
-  ps -p $PID > /dev/null 2>&1
-  if [ $? -eq 0 ]
-  then
+  ps -p $PID >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
     echo "Process already running"
     exit 1
   else
     ## Process not found assume not running
-    echo $$ > $PIDFILE
-    if [ $? -ne 0 ]
-    then
+    echo $$ >$PIDFILE
+    if [ $? -ne 0 ]; then
       echo "Could not create PID file"
       exit 1
     fi
   fi
 else
-  echo $$ > $PIDFILE
-  if [ $? -ne 0 ]
-  then
+  echo $$ >$PIDFILE
+  if [ $? -ne 0 ]; then
     echo "Could not create PID file"
     exit 1
   fi
 fi
 
-
 # Main script ... Do work here
-
-#while inotifywait -r -e modify,create,move ./samples; do
-  rsync --remove-source-files -rcvhP samples tim@mohiohio.com:buzzy --log-file=upload.log
-#done
-
+rsync --remove-source-files -rcvhP samples tim@mohiohio.com:buzzy --log-file=upload.log
 # End
 
-
-#echo "shutdown tailscale"
-#/usr/bin/tailscale down --accept-risk=lose-ssh
-#sleep 30
-
-#echo "shutdown usb"
-#sleep 2
-#/usr/sbin/uhubctl -l 1-1 -p 2 -a off
-
-
 rm $PIDFILE
-
