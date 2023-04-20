d60e3a92fee620b7848b24d56a8d6eed72f76631
Tim Field <tim@mohiohio.com>
2023-04-20 20:19:29 +1200

    run.sh updates

--- run.sh
+++ run.sh
@@ -3,12 +3,6 @@ cd "$(dirname "$0")"
 
 trap "pkill -P $$" SIGINT SIGTERM EXIT
 
-# echo "startup usb"
-# /usr/sbin/uhubctl -l 1-1 -p 2 -a on
-# sleep 90
-# /usr/bin/tailscale up
-./usb-on.sh
-
 # 20 minutes default
 duration=${1:-1200}
 
@@ -18,12 +12,3 @@ duration=${1:-1200}
 wait
 echo "jobs done"
 
-./usb-off.sh
-
-# echo "shutdown tailscale"
-# /usr/bin/tailscale down --accept-risk=lose-ssh
-# sleep 30
-
-# echo "shutdown usb"
-# sleep 2
-# /usr/sbin/uhubctl -l 1-1 -p 2 -a off
